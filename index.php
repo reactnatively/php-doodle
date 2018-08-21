a9237fa8c0b68ca24a4d3c979d4c907056b78496
Adolphus Nolan <al@Adolphuss-MacBook-Pro.local>
2018-08-21 14:17:07 -0500

    creating our index page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+</head>
+<body>
+
+
+	<div class="mainSection">
+
+		<div class="logoContainer">
+			<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+		</div>
+
+
+		<div class="searchContainer">
+
+			<input class="searchBox" type="text" name="term">
+			<input class="searchButton" type="submit" value="Search">
+
+		</div>
+
+
+	</div>
+
+
+
+
+</body>
+</html>
