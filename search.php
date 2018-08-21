0ebf0d95bafa2164b27bbd24e2ea910a3f412708
Adolphus Nolan <al@Adolphuss-MacBook-Pro.local>
2018-08-21 14:56:46 -0500

    create our searh page structure

--- search.php
+++ search.php
@@ -1,5 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
 
-echo $_GET["term"];
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-?>
+</head>
+<body>
+
+	<div class="wrapper">
+	
+		<div class="header">
+
+
+			<div class="headerContent">
+
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/doodleLogo.png">
+					</a>
+				</div>
+
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+
+						<div class="searchBarContainer">
+
+							<input class="searchBox" type="text" name="term">
+							<button>
+								Search
+							</button>
+						</div>
+
+
+
+
+					</form>
+
+
+				</div>
+
+
+
+			</div>
+
+
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
