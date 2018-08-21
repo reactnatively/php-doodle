45a24267e477e31219f2208a6ed1f5f2d5cca234
Adolphus Nolan <al@Adolphuss-MacBook-Pro.local>
2018-08-21 15:03:37 -0500

    document parser class

---
+++ crawl.php
@@ -0,0 +1,13 @@
+<?php
+include("classes/DomDocumentParser.php");
+
+function followLinks($url) {
+
+	$parser = new DomDocumentParser($url);
+
+
+}
+
+$startUrl = "http://www.reecekenney.com";
+followLinks($startUrl);
+?>
