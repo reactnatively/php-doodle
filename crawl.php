80b2c501d912b9e557d1d5171d32b6039383db13
Adolphus Nolan <al@Adolphuss-MacBook-Pro.local>
2018-08-21 17:48:23 -0500

    continuing with extractions

--- crawl.php
+++ crawl.php
@@ -6,16 +6,21 @@ function createLink($src, $url) {
 	$scheme = parse_url($url)["scheme"]; // http
 	$host = parse_url($url)["host"]; // www.reecekenny.com
 
-	if(substr($src,0,2) == "//") {
+	if(substr($src, 0, 2) == "//") {
 		$src = $scheme . ":" . $src;
 	}
-	else if(substr($src,0,1) == "/") {
-			$src = $scheme . "://" . $host . $src;
-		}
-	/*
-	echo "SRC: $src<br/>";
-	echo "URL: $url<hr/>";
-	*/
+	else if(substr($src, 0, 1) == "/") {
+		$src = $scheme . "://" . $host . $src;
+	}
+	else if(substr($src, 0, 2) == "./") {
+		$src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src,1);
+	}
+	else if(substr($src, 0, 3) == "../") {
+		$src = $scheme . "://" . $host . "/" . $src;
+	}
+	else if(substr($src, 0, 5) != "https" && (substr($src, 0, 4) != "http")) {
+		$src = $scheme . "://" . $host . "/" . $src;
+	}
 
 	return $src;
 
