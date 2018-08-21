d0731cd7052b0fa6333f3227bdaaf68a16e52699
Adolphus Nolan <al@Adolphuss-MacBook-Pro.local>
2018-08-21 15:22:51 -0500

    ignoring links we don't want to use

--- crawl.php
+++ crawl.php
@@ -1,6 +1,11 @@
 <?php
 include("classes/DomDocumentParser.php");
 
+function createLink($src, $url) {
+	echo "SRC: $src<br/>";
+	echo "URL: $url<hr/>";
+}
+
 function followLinks($url) {
 
 	$parser = new DomDocumentParser($url);
@@ -8,12 +13,22 @@ function followLinks($url) {
 	$linkList = $parser->getLinks();
 
 	foreach($linkList as $link) {
+
 		$href = $link->getAttribute("href");
-		echo $href . "<br>";
+		if(strpos($href,'#') !== false) {
+			continue;
+		}
+		else if(substr($href,0,11) == "javascript:") {
+			continue;
+		}
+
+		createLink($href, $url);
+		//echo $href . "<br>";
+
 	}
 
 }
 
-$startUrl = "http://www.apple.com";
+$startUrl = "http://www.bbc.com";
 followLinks($startUrl);
 ?>
