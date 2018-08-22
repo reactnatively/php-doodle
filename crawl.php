9c5ce47768e0c89f485a7384036d50b9fd4c681d
Adolphus Nolan <al@Adolphuss-MacBook-Pro.local>
2018-08-21 22:21:18 -0500

    prevent duplicate entries

--- crawl.php
+++ crawl.php
@@ -8,7 +8,22 @@ include("classes/DomDocumentParser.php");
 $alreadyCrawled = array();
 $crawling = array();
 
+	//
+function linkExists($url) {
+
+	global $con;
+
+	$query = $con->prepare("SELECT * FROM sites WHERE url = :url");
+
+	$query->bindParam(":url", $url);
+	$query->execute();
+
+	return $query->rowCount() != 0;
+
+}
+
 function insertLink($url, $title, $description, $keywords) {
+
 	global $con;
 
 	$query = $con->prepare("INSERT INTO sites(url, title, description, keywords) VALUES (:url, :title, :description, :keywords)");
@@ -83,7 +98,15 @@ function getDetails($url) {
 	$description = str_replace("\n", "", $description);
 	$keywords = str_replace("\n", "", $keywords);
 
-	insertLink($url, $title, $description, $keywords);
+	if(linkExists($url)) {
+		echo "The URL $url already exists<br/>";
+	}
+	else if(insertLink($url, $title, $description, $keywords)) {
+		echo "SUCCESS: $url<br/>";
+	}
+	else {
+		echo "ERROR: Failed to insert<br/>";
+	}
 
 	echo "URL: $url<br/> Description: $description<br/> Keywords: $keywords<hr/>";
 
